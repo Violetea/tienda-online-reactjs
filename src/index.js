e47c502b5e899e0e88a4beb53054c76c31714cf7
Violetea <andrealamusvkj@hotmail.com>
2022-11-13 11:51:21 +0100

    bootstrap add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+
+// Añadimos Bootstrap a nuestro proyecto
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+// ! Importante: los estilos propios, deben ir debajo del bootstrap para que no los pise.
 import './index.css';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
